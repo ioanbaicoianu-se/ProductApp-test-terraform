df17b83039017617ce3bee93f0bbe7a5a03ee8e3
Ioan <ioan.baicoianu@se.com>
2024-09-17 13:25:47 +0200

    changed the db crendetials

--- ProductApp/ProductService.cs
+++ ProductApp/ProductService.cs
@@ -7,7 +7,7 @@ namespace ProductApp
 {
     public class ProductService
     {
-        private static string db_connectionstring = "server=appserver6008089.database.windows.net;user=sqladmin;password=Azure@123;database=appdb";
+        private static string db_connectionstring = "server=terraformdaffwsqlserver123134.database.windows.net;user=sqladmin;password=Azure@123;database=terraformdaffwsqldatabase310831";
 
         private SqlConnection GetConnection()
         {
